fcdd76e5b02e82c61cb8751cd78b5a4d0644e982
Didac <didac.magrina@gmail.com>
2017-07-06 17:30:39 +0100

    Stub routines created

--- FHIR-MAPPING/src/main/java/ConverterOpenempi.java
+++ FHIR-MAPPING/src/main/java/ConverterOpenempi.java
@@ -1,27 +1,119 @@
 package fhirconverter;
 
 public class ConverterOpenempi{
-	public static String main(String [] agrs){
+
+
+
+
+	public String practitionerRead(String id,  Representation format) {
+
+
+		return "";
+	}
+	public String patientRead(String id, Representation format) {
+
+
+
+
+		return "";
+	}
+	public String groupRead(String id, Representation format) {
+
+
+		return "";
+	}
+
+	public String practitionerSearch(String parameters, Representation format) {
+
+
+		return "";
+	}
+
+	public String patientSearch(String parameters, Representation format) {
+
+		return "";
+	}
+	public String groupSearch(String parameters, Representation format) {
+
+
+		return "";
+	}
+	public String patientUpdate(String id, Representation format) {
+
+		return "";
+	}
+	public String practitionerUpdate(String id, Representation format) {
+
+		return "";
+	}
+	public String groupUpdate(String id, Representation format) {
+
+		return "";
+	}
+	public String patientPatch(String id, Representation format) {
+
+		return "";
+	}
+	public String groupPatch(String id, Representation format) {
+
+		return "";
+	}
+	public String practitionerPatch(String id, Representation format) {
+
+		return "";
+	}
+	public String patientCreate(Representation format) {
+
+		return "";
+	}
+	public String practitionerCreate(Representation format) {
+
+		return "";
+	}
+	public String groupCreate(Representation format) {
+
+		return "";
+	}
+	public String patientDelete(String id) {
+
+		return "";
+	}
+	public String practitionerDelete(String id) {
+
+		return "";
+	}
+	public String groupDelete(String id) {
+
+		return "";
+	}
+
+
+
+
+
+	public static void main(String [] agrs){
 		//string type = agrs[0]; // 0 - patient, 1 - practitioner, 2- group
-	        PropertiesApp properties = new PropertiesApp();
-		properties.createProperties();		
-		int typeTesting = 0; // remove this 
+
+		PropertiesApp properties = new PropertiesApp();
+		properties.createProperties();
+		int typeTesting = 0; // remove this
+
 		//String connection = agrs[1];
 		OpenEMPIbase mapper;
-		String testing = "Initial Testing";
 		switch(typeTesting){
-			case 0: 	
+			case 0:
 				mapper = new PatientFHIR();
-				testing = mapper.convertFHIR();
+				String testing = mapper.convertFHIR();
+				System.out.println(testing);
 				break;
-			case 1: 
-		  		mapper = new PractitionerFHIR();
+			case 1:
+				mapper = new PractitionerFHIR();
 				break;
-			case 2: 
+			case 2:
 				mapper = new GroupFHIR();
 				break;
+			default: break;
 		}
-		return testing;
 
 	}
 
