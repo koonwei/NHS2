1608b51e9692ea0bcdb7ae2165ead02dc5c1bffd
koon wei <koon wei>
2017-07-26 18:30:29 +0100

    Fixed refresh Token

--- FHIR-MAPPING/src/main/java/fhirconverter/converter/OpenEHRConnector.java
+++ FHIR-MAPPING/src/main/java/fhirconverter/converter/OpenEHRConnector.java
@@ -21,14 +21,13 @@ public class OpenEHRConnector{
 		password =  connectionCreds.get("password");
 		sessionCode = getSessionCode();
 	}
-	public String getSessionCode() throws Exception{
+	private String getSessionCode() throws Exception{
 		if(sessionCode == null){
 			try{
 				HttpResponse<String> response = Unirest.post(baseURL+"/rest/v1/session?"+"username="+username+"&password="+password)
   								.asString(); 
 				JSONObject responseObj = new JSONObject(response.getBody());
 				if(responseObj.has("sessionId")){
-					System.out.println(responseObj.getString("sessionId"));
 					return responseObj.getString("sessionId");
 			 	}else{
 					throw new IdNotObtainedException("Session ID not Created!");
@@ -37,18 +36,9 @@ public class OpenEHRConnector{
 				e.printStackTrace();
 			}
 		}	
-		else {
-			   Boolean refreshTokenFlag = refreshSessionCode(sessionCode);
-			   if(refreshTokenFlag == true){
-			   	return sessionCode;
-			   }else{
-				sessionCode = null;
-				getSessionCode();
-			   }
-		}	
 		return null;
 	}
-	public Boolean refreshSessionCode(String sessionId) {
+	private Boolean refreshSessionCode(String sessionId) {
 		try{
 			HttpResponse<String> response = Unirest.put(baseURL+"/rest/v1/session")
   							.header("ehr-session", sessionId)
@@ -87,7 +77,16 @@ public class OpenEHRConnector{
 		}
 		
 	}
+	private void ensureSessionValidity() throws Exception{
+		Boolean refreshTokenFlag = refreshSessionCode(sessionCode);
+		if(refreshTokenFlag == false){
+			sessionCode = null;
+			getSessionCode();
+		}
+
+	}
 	public JSONObject getGrowthChartObservations(String nhsNumber) throws Exception{
+		ensureSessionValidity();
 		String ehrID = getEHRIdByNhsNumber(nhsNumber);
 		String aqlString = "select%20b_a%2Fdata%5Bat0002%5D%2Forigin%2Fvalue%20as%20Weight_date%2C%20b_a%2Fdata%5Bat0002%5D%2Fevents%5Bat0003%5D%2Fdata%5Bat0001%5D%2Fitems%5Bat0004%5D%2Fvalue%2Fmagnitude%20as%20Weight_magnitude%2C%20b_a%2Fdata%5Bat0002%5D%2Fevents%5Bat0003%5D%2Fdata%5Bat0001%5D%2Fitems%5Bat0004%5D%2Fvalue%2Funits%20as%20Weight_units%2C%20b_b%2Fdata%5Bat0001%5D%2Forigin%2Fvalue%20as%20Height_Length_date%2C%20b_b%2Fdata%5Bat0001%5D%2Fevents%5Bat0002%5D%2Fdata%5Bat0003%5D%2Fitems%5Bat0004%5D%2Fvalue%2Fmagnitude%20as%20Height_Length_magnitude%2C%20b_b%2Fdata%5Bat0001%5D%2Fevents%5Bat0002%5D%2Fdata%5Bat0003%5D%2Fitems%5Bat0004%5D%2Fvalue%2Funits%20as%20Height_Length_units%2C%20b_c%2Fdata%5Bat0001%5D%2Forigin%2Fvalue%20as%20Head_circumference_date%2C%20b_c%2Fdata%5Bat0001%5D%2Fevents%5Bat0010%5D%2Fdata%5Bat0003%5D%2Fitems%5Bat0004%5D%2Fvalue%2Fmagnitude%20as%20Head_circumference_magnitude%2C%20b_c%2Fdata%5Bat0001%5D%2Fevents%5Bat0010%5D%2Fdata%5Bat0003%5D%2Fitems%5Bat0004%5D%2Fvalue%2Funits%20as%20Head_circumference_units%2C%20b_d%2Fdata%5Bat0001%5D%2Forigin%2Fvalue%20as%20Body_Mass_index_date%2C%20b_d%2Fdata%5Bat0001%5D%2Fevents%5Bat0002%5D%2Fdata%5Bat0003%5D%2Fitems%5Bat0004%5D%2Fvalue%2Fmagnitude%20as%20Body_Mass_Index_magnitude%2C%20b_d%2Fdata%5Bat0001%5D%2Fevents%5Bat0002%5D%2Fdata%5Bat0003%5D%2Fitems%5Bat0004%5D%2Fvalue%2Funits%20as%20Body_Mass_Index_units%2C%20b_f%2Fdata%5Bat0001%5D%2Forigin%2Fvalue%20as%20Skeletal_age_date%2C%20b_f%2Fdata%5Bat0001%5D%2Fevents%5Bat0002%5D%2Fdata%5Bat0003%5D%2Fitems%5Bat0005%5D%2Fvalue%2Fvalue%20as%20Skeletal_age%20from%20EHR%20%5Behr_id%2Fvalue%3D'" +ehrID +"'%5D%20contains%20COMPOSITION%20a%5BopenEHR-EHR-COMPOSITION.report.v1%5D%20contains%20(%20OBSERVATION%20b_a%5BopenEHR-EHR-OBSERVATION.body_weight.v1%5D%20or%20OBSERVATION%20b_b%5BopenEHR-EHR-OBSERVATION.height.v1%5D%20or%20OBSERVATION%20b_c%5BopenEHR-EHR-OBSERVATION.head_circumference.v0%5D%20or%20OBSERVATION%20b_d%5BopenEHR-EHR-OBSERVATION.body_mass_index.v1%5D%20or%20OBSERVATION%20b_f%5BopenEHR-EHR-OBSERVATION.skeletal_age.v0%5D)";
 		try{
