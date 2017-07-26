35c57006cfd852b2f74cfc0b0dca2fe77ac014ee
koon wei <koon wei>
2017-07-26 17:03:20 +0100

    OpenEHR connector (Get Session, Get EHRId, GetObs)

---
+++ FHIR-MAPPING/src/test/java/fhirconverter/converter/OpenEHRConnectorTests.java
@@ -0,0 +1,31 @@
+package fhirconverter.converter;
+import org.junit.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import fhirconverter.exceptions.IdNotObtainedException;
+import org.json.JSONObject;
+import org.json.JSONArray;
+
+public class OpenEHRConnectorTests{
+	@Test
+	public void getEHRidNumberTest() throws Exception{
+		OpenEHRConnector tester = new OpenEHRConnector();		
+		assertEquals("Get EHRid operation","c831fe4d-0ce9-4a63-8bfa-2c51007f97e5",tester.getEHRIdByNhsNumber("9999999332"));
+	}
+	@Test(expected = IdNotObtainedException.class)
+	public void getEHRidNumberNotExistTest() throws Exception{	
+		OpenEHRConnector tester = new OpenEHRConnector();	
+		tester.getEHRIdByNhsNumber("999999933");
+	}
+	@Test
+	public void getObservationsTests() throws Exception{
+		OpenEHRConnector tester = new OpenEHRConnector();	
+		JSONObject responseObj = tester.getGrowthChartObservations("9999999332");
+		if(responseObj.has("resultSet"))
+		{
+			assertNotNull(responseObj.getJSONArray("resultSet"));
+		}
+	}
+}
+
+
