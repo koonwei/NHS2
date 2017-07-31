e07fce688c4f13660cf6b8e67465d8cc0677fc92
koon wei <koon wei>
2017-07-31 16:06:23 +0100

    fix minor bugs

--- FHIR-MAPPING/src/main/java/fhirconverter/converter/ObservationFHIR.java
+++ FHIR-MAPPING/src/main/java/fhirconverter/converter/ObservationFHIR.java
@@ -1,17 +1,19 @@
 package fhirconverter.converter; 
-import fhirconverter.utilites.PatientHelper; 
+import fhirconverter.utilities.PatientHelper; 
 
 import ca.uhn.fhir.model.dstu2.resource.Observation;
 
 import org.json.JSONObject;
-
+import java.util.List;
 public class ObservationFHIR{
 	PatientHelper patientHelper = new PatientHelper();
-	protected List<Observation> search(String patientId) throws Exception{
+
+	protected List<Observation> search(String patientId, String domainName) throws Exception{ 
 		String nhsNumber = patientHelper.retrieveNHSbyId(patientId);
-		OpenEHRConnector openEHRconnector = new OpenEHRConnector(); // Future developers, Note this line of code is placed here to be thread safe.
-		JSONObject observationObj = openEHRconnector.getGrowthChartObservations();			
+		OpenEHRConnector openEHRconnector = new OpenEHRConnector(domainName); // Future developers, Note this line of code is placed here to be thread safe.
+		JSONObject observationObj = openEHRconnector.getGrowthChartObservations(domainName);			
 		//Add Shruti's call for mapper!
+		return null;
 		
 	}
 
