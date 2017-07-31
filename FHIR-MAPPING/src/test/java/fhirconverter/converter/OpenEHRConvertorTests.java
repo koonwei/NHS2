01b5ec5caa51f6f433d67579beb11eb34ac1ea54
Shruti <shruti22sinha@gmail.com>
2017-07-31 18:23:20 +0100

    serach testcase fixed

---
+++ FHIR-MAPPING/src/test/java/fhirconverter/converter/OpenEHRConvertorTests.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package fhirconverter.converter;
+
+import static org.junit.Assert.*;
+
+import org.json.JSONObject;
+import org.junit.Test;
+
+/**
+ * Test Cases to test functionalities of the class OpenEHRConvertor
+ * 
+ * @author Shruti Sinha
+ *
+ */
+public class OpenEHRConvertorTests {
+
+	@Test
+	public void testConversion() throws Exception {
+		
+	}
+}
