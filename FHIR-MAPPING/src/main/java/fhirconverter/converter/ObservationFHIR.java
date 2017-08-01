ce51cc10ec08cf3a45c9f10ddfcd73167278e203
koon wei <koon wei>
2017-08-01 12:54:40 +0100

    FHIR-Handler Completed

--- FHIR-MAPPING/src/main/java/fhirconverter/converter/ObservationFHIR.java
+++ FHIR-MAPPING/src/main/java/fhirconverter/converter/ObservationFHIR.java
@@ -5,16 +5,23 @@ import ca.uhn.fhir.model.dstu2.resource.Observation;
 
 import org.json.JSONObject;
 import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class ObservationFHIR{
 	PatientHelper patientHelper = new PatientHelper();
+	Logger LOGGER = LogManager.getLogger(ObservationFHIR.class);
 
 	protected List<Observation> search(String patientId, String domainName) throws Exception{ 
 		String nhsNumber = patientHelper.retrieveNHSbyId(patientId);
+		LOGGER.info("nhsNumber" + nhsNumber);
 		OpenEHRConnector openEHRconnector = new OpenEHRConnector(domainName); // Future developers, Note this line of code is placed here to be thread safe.
-		JSONObject observationObj = openEHRconnector.getGrowthChartObservations(domainName);			
-		//Add Shruti's call for mapper!
-		return null;
-		
+		JSONObject observationObj = openEHRconnector.getGrowthChartObservations(nhsNumber);			
+		observationObj.put("patientId", patientId);
+		LOGGER.info("observationObj " + observationObj.toString(3));
+		OpenEHRConvertor openEHRconvertor = new OpenEHRConvertor();
+		return openEHRconvertor.jSONToObservation(observationObj);
 	}
 
 }
---
+++ FHIR-MAPPING/src/test/java/fhirconverter/converter/ObservationFHIRTests.java
@@ -0,0 +1,38 @@
+package fhirconverter.converter;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.json.JSONObject;
+import org.junit.Test;
+
+import fhirconverter.exceptions.IdNotObtainedException;
+
+import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.model.dstu2.resource.Observation;
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class ObservationFHIRTests{
+	Logger LOGGER = LogManager.getLogger(ObservationFHIRTests.class);
+	@Test
+	public void getObservationIntegrationTest() throws Exception{
+		ObservationFHIR observationFHIR = new ObservationFHIR();
+		List<Observation> observations = observationFHIR.search("3185", "openEhrApi");
+		FhirContext ctx = FhirContext.forDstu2();
+		for(Observation observation : observations){
+			String resourceJson = ctx.newJsonParser().encodeResourceToString(observation);	
+			JSONObject obtained_object = new JSONObject(resourceJson);	
+			LOGGER.info(obtained_object.toString(3));	
+			assertNotNull(observation);
+		}
+	}
+}
+
+
+
+
+
+
+
