02bf8209827780dfd0b7c3f309ffccbde8a4a439
Yuan <yw.johnny@gmail.com>
2017-08-11 17:17:56 +0100

    Complete observation search scenario

--- fhir_testing/features/step_definitions/common_steps.rb
+++ fhir_testing/features/step_definitions/common_steps.rb
@@ -1,31 +1,33 @@
-server_base = 'http://localhost:8090/fhir'
-
 Given(/^fixture "([^"]*)" has loaded as "([^"]*)"$/) do |file_name, variable_name|
-  json_file = File.read(file_name)
+  json_file = File.read(File.join('fixtures', file_name))
   json_hash = JSON.parse(json_file)
 
   instance_variable_set("@#{variable_name}", json_hash)
-  @created_patients = Array.new(0)
 end
 
 Given(/^a patient was created using "([^"]*)" fixture, with family name "([^"]*)", given name "([^"]*)", gender "([^"]*)", birthDate "([^"]*)", and NHS identifier "([^"]*)"$/) do |variable_name, family_name, given_name, gender, birthdate, nhs_id|
   #clone method doesn't deep copy json arrays (like name)
   patient = JSON.parse(instance_variable_get("@#{variable_name}").to_json)
-  patient['name'].first['family'] = []
-  patient['name'].first['family'] << family_name
-  patient['name'].first['given'] = []
-  patient['name'].first['given'] << given_name
+
+  patient['name'].first['family'] = [family_name]
+  patient['name'].first['given'] = [given_name]
   patient['gender'] = gender
   patient['birthDate'] = birthdate
   patient['identifier'][0]['value'] = nhs_id
 
-  payload = patient.to_json
   begin
-    response = RestClient.post server_base + '/Patient', payload, :content_type => 'application/json', :accept => :json
+    response = RestClient.post  "#{$fhir_server_base}/Patient", patient.to_json, :content_type => 'application/json', :accept => :json
     location = response.headers[:location]
     patient['id'] = location.scan(/Patient\/(\d+)/).first.first
-    @created_patients << patient.clone
+    if @created_patients.nil? then
+      @created_patients = Array.new
+    end
+    @created_patients << patient
   rescue StandardError => e
     puts e.response
   end
+end
+
+Then(/^The server response has status code (\d+)$/) do |code|
+  expect(@response.code).to eq(code.to_i)
 end
--- fhir_testing/features/step_definitions/observation_steps.rb
+++ fhir_testing/features/step_definitions/observation_steps.rb
@@ -1,21 +1,73 @@
 require 'rest-client'
 require 'json'
 require 'json-diff'
-require 'crack/xml'
 require "rspec"
 include RSpec::Matchers
 
-server_base = 'http://localhost:8090/fhir/'
-test_operonBase = 'https://test.operon.systems/rest/v1/ehr/'
-test_operonCompositionBase = "https://test.operon.systems/rest/v1/composition?"
-{{openEhrApi}}/rest/v1/composition?ehrId={{ehrId}}&templateId={{templateId}}&committerName={{committerName}}&format=FLAT
+@ehr_username = 'oprn_jarrod'
+@ehr_password = 'ZayFYCiO644'
+
+def get_session(username, password)
+    url = "#{$ehr_server_base}/session?username=#{username}&password=#{password}"
+    response = RestClient.post url, :content_type => :json, :accept => :json
+    return JSON.parse(response.body)['sessionId']
+end
+
+$session_id = get_session(@ehr_username, @ehr_password)
+
+def add_quantity_date(value, unit, prefix)
+    return { "#{prefix}|magnitude" => value, "#{prefix}|unit" => unit }
+end
+
+def new_composition(date, weight, height, bmi, head_circumference)
+    record = { "ctx/language" => "en",
+      "ctx/territory" => "GB",
+     "ctx/composer_name" => "Dummy",
+     "ctx/time" => "#{date}",
+     "ctx/id_namespace" => "Hospital",
+     "ctx/id_scheme" => "HOSPITAL-NS"}
+
+    if(weight != 'nil') then 
+        record = record.merge(add_quantity_date(weight, 'kg', 'smart_growth_report/body_weight/weight')) 
+    end
+
+    if(height != 'nil') then 
+        record = record.merge(add_quantity_date(height, 'cm', 'smart_growth_report/height_length/any_event:0/height_length'))
+    end
+
+    if(bmi != 'nil') then 
+        record = record.merge(add_quantity_date(bmi, 'kg/m2', 'smart_growth_report/body_mass_index/any_event:0/body_mass_index'))
+    end
+
+    if(head_circumference != 'nil') then 
+        record= record.merge(add_quantity_date(head_circumference, 'cm', 'smart_growth_report/head_circumference/any_event:0/head_circumference'))
+    end
+
+    return record
+end
+
+def commit_composition(composition, ehrId, sessionId)
+    url = "#{$ehr_server_base}/composition?ehrId=#{ehrId}&templateId=Smart Growth Chart Data.v0&format=FLAT"
+    begin 
+        response = RestClient.post url, composition.to_json, :content_type => :json, 'Ehr-Session' => sessionId
+        composition_id = JSON.parse(response.body)['compositionUid']
+        if $created_compositions.nil? then
+            $created_compositions = Array.new
+        end
+        $created_compositions << composition_id
+    rescue StandardError => e
+        puts e.response.body
+    end
+end
 
 def compare_observations (expected_observation, obtained_observation)
     [expected_observation, obtained_observation].each do |o|
         o.delete("meta")
         o.delete("id")
+        o.delete("link")
         o['entry'].each do |e|
             e.delete('fullUrl')
+            e['resource'].delete('subject')
             e['resource'].delete('id')
         end
     end
@@ -23,83 +75,48 @@ def compare_observations (expected_observation, obtained_observation)
     return (JsonDiff.diff(obtained_observation,expected_observation))
 end
 
-def create_weight_payload (value, date)
-    payload = '{
-    "ctx/language": "en",
-    "ctx/territory": "GB",
-    "ctx/composer_name": "Nurse Crachett",
-    "ctx/time": "#{date}",
-    "ctx/id_namespace": "Hospital",
-    "ctx/id_scheme": "HOSPITAL-NS",
-    "ctx/health_care_facility|name": "Marandia DGH",
-    "ctx/health_care_facility|id": "9095",
-    "smart_growth_report/body_weight/weight|magnitude": #{value},
-    "smart_growth_report/body_weight/weight|unit": "kg"}'
-    return (payload)
-end    
-
-def create_height_payload (value, date)
-    payload = '{
-    "ctx/language": "en",
-    "ctx/territory": "GB",
-    "ctx/composer_name": "Nurse Crachett",
-    "ctx/time": "#{date}",
-    "ctx/id_namespace": "Hospital",
-    "ctx/id_scheme": "HOSPITAL-NS",
-    "ctx/health_care_facility|name": "Marandia DGH",
-    "ctx/health_care_facility|id": "9095",
-    "smart_growth_report/height_length/any_event:0/height_length|magnitude": #{value},
-    "smart_growth_report/height_length/any_event:0/height_length|unit": "cm"}'
-    return (payload)
-end    
-
-def create_bmi_payload (value, date)
-    payload = '{
-    "ctx/language": "en",
-    "ctx/territory": "GB",
-    "ctx/composer_name": "Nurse Crachett",
-    "ctx/time": "#{date}",
-    "ctx/id_namespace": "Hospital",
-    "ctx/id_scheme": "HOSPITAL-NS",
-    "ctx/health_care_facility|name": "Marandia DGH",
-    "ctx/health_care_facility|id": "9095",
-        "smart_growth_report/body_mass_index/any_event:0/body_mass_index|magnitude": #{value},
-    "smart_growth_report/body_mass_index/any_event:0/body_mass_index|unit": "kg/m2"
-    }'
-
-end  
-
 Given(/^an EHRid exists for the subjec id "([^"]*)"$/) do |subject_id|
-    url = test_operonBase + "?subjectId=#{subject_id}&subjectNamespace=uk.nhs.nhs_number"
-    @response = RestClient.get url, :content_type => :json, :accept => :json
-    if @response.code == 200
-      replyJSON = JSON.parse(@response.body)
-      @ehrid = replyJSON['"ehrId"']
-    end
-    if @response.code == 204
-      replyJSON = JSON.parse(@response.body)
-      @response = RestClient.post url, :content_type => :json, :accept => :json
-      replyJSON = JSON.parse(@response.body)
-      @ehrid = replyJSON['"ehrId"']
+    # url = "#{$ehr_server_base}/ehr?subjectId=#{subject_id}&subjectNamespace=https://fhir.nhs.uk/Id/nhs-number"
+    url = "#{$ehr_server_base}/ehr?subjectId=#{subject_id}&subjectNamespace=uk.nhs.nhs_number"
+    response = RestClient.get url, :content_type => :json, :accept => :json, 'Ehr-Session' => $session_id
+    if response.code == 200
+        @ehrid = JSON.parse(response.body)['ehrId']
+        # puts @ehrid
+    elsif response.code == 204
+        response = RestClient.post url, nil, :content_type => :json, :accept => :json, 'Ehr-Session' => $session_id
+        @ehrid = JSON.parse(response.body)['ehrId']
+        # puts @ehrid
     end
 end
 
-Given(/^Create composition for "([^"]*)" observation with value "([^"]*)" and date "([^"]*)"$/) do |observation, value, date|
-    url = test_operonCompositionBase + "ehrId=#{ehrid}&templateId=Smart%20Growth%20Chart%20Data.v0&format=FLAT"
-    
-    
+And(/^the following compositions are created with the same ehrid:$/) do |table|
+    table.hashes.each do |record|
+        composition = new_composition(record[:date], record[:weight], record[:height], record[:bmi], record[:head_circumference])
+        commit_composition(composition, @ehrid, $session_id)
+    end
 end
 
-
-When(/^I search Height, Weight, BMI, Head Circumference observation with Subject Id "([^"]*)"$/) do |subject_id|
-    url = server_base + "Observation?code=3141-9,8302-2,8287-5,58941-6&patient=#{subject_id}&_format=json"
+When(/^I search Height, Weight, BMI, Head Circumference observation with the created patient id$/) do
+    patient_id = @created_patients.first['id']
+    url = "#{$fhir_server_base}/Observation?code=http://loinc.org|3141-9,http://loinc.org|8302-2,http://loinc.org|8287-5,http://loinc.org|58941-6&patient=#{patient_id}&_format=json"
     @response = RestClient.get url, :content_type => :json, :accept => :json
 end
 
 And(/^the response is a bundle that contains the Observations created$/) do
-    bundle = JSON.parse(@response.body)   
+    bundle = JSON.parse(@response.body)
     expected_observations = @observations
     expect(bundle).not_to be_nil 
     diff = compare_observations(expected_observations, bundle)
     expect(diff).to eq([])
+end
+
+Then(/^all compositions created are deleted$/) do
+    aql = "select a/uid from EHR [ehr_id/value='#{@ehrid}'] contains COMPOSITION a"
+    url = "#{$ehr_server_base}/query?aql=#{aql}"
+    response = RestClient.get url, :content_type => :json, :accept => :json, 'Ehr-Session' => $session_id
+    JSON.parse(response.body)['resultSet'].each do |c|
+        id = c['#0']['value']
+        url = "#{$ehr_server_base}/composition/#{id}"
+        RestClient.delete url, :content_type => :json, :accept => :json, 'Ehr-Session' => $session_id
+    end
 end
