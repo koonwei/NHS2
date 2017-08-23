36d4eeb819686737bf4c3729bee71eef02d5dbfd
Didac <didac.magrina@gmail.com>
2017-08-23 14:18:03 +0100

    Assert for patient_name added

--- fhir_testing/StabilityEvaluation.rb
+++ fhir_testing/StabilityEvaluation.rb
@@ -1,11 +1,14 @@
 require "selenium-webdriver"
+require "test/unit"
+
+extend Test::Unit::Assertions
 
 driver = Selenium::WebDriver.for :chrome
 driver.navigate.to "http://51.140.57.74:3000/growth-chart/id/1"
 
+#maybe the method "wait" from selenium gem can be used to stop execution until some element appears
 sleep(10)
 patient_name = driver.find_element(:css, "span.patient-name.title")
-puts patient_name.text
-
-puts driver.title
+assert_equal patient_name.text, "Keira Jones"
+puts "Data Retrieved Successfully"
 
