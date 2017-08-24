99f8efcca4eae1e4b292aec2db18bd324902f94e
Didac <didac.magrina@gmail.com>
2017-08-24 11:29:44 +0100

    length assert added

--- fhir_testing/StabilityEvaluation.rb
+++ fhir_testing/StabilityEvaluation.rb
@@ -3,12 +3,19 @@ require "test/unit"
 
 extend Test::Unit::Assertions
 
+length_expected_list = ["177", "82.3", "86.3", "90.1cm"]
+
+def check_heading_elements(heading, expected_elements)
+	obtained_elements = heading.find_elements(:css, "td")
+	obtained_elements.zip(expected_elements).each do |obtained_element, expected_element|
+		assert_equal obtained_element.text, expected_element 
+	end
+end
+
 driver = Selenium::WebDriver.for :chrome
 driver.navigate.to "http://51.140.57.74:3000/growth-chart/id/1"
 
-
-#maybe the method "wait" from selenium gem can be used to stop execution until some element appears
-sleep(2)
+sleep(4)
 
 patient_name = driver.find_element(:css, "span.patient-name.title")
 assert_equal patient_name.text, "Keira Jones", "Patient name retrieved incorrectly"
@@ -18,6 +25,12 @@ assert_equal patient_gender.text, "female", "Patient gender retrieved incorrectl
 
 patient_dob = driver.find_element(:css, "span.patient-birth.title")
 assert_equal patient_dob.text, "23Mar2009", "Patient date of birth retrieved incorrectly"
+
+
+driver.find_element(:xpath, '//span[@data-value="table"]').click
+
+length_heading = driver.find_element(:css, "tr.length.heading")
+check_heading_elements(length_heading, length_expected_list)
 
 puts "Data Retrieved Successfully"
 
